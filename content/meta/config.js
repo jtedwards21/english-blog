860bb0f256c858efd9821a026048e2437c84c363
Joshua Edwards <joshuaedwards@Joshuas-MacBook-Pro.local>
2018-10-27 21:12:18 +0800

    Edit: about and Resources

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,32 +1,31 @@
 const colors = require("../../src/styles/colors");
 
 module.exports = {
-  siteTitle: "PersonalBlog - a blog starter for GatsbyJS", // <title>
-  shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
-  siteDescription: "PersonalBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
+  siteTitle: "SpeakUp - An English Blog", // <title>
+  shortSiteTitle: "SpeakUp", // <title> ending for posts and pages
+  siteDescription: "SpeakUp is a Blog for English Language Learning.",
+  siteUrl: "https://speakup.com",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "joshua edwards",
+  authorTwitterAccount: "speakup",
   // info
-  infoTitle: "greg lobinski",
-  infoTitleNote: "personal blog",
+  infoTitle: "SpeakUp",
+  infoTitleNote: "A Language Learning Blog",
   // manifest.json
-  manifestName: "PersonalBlog - a blog starter for GatsbyJS",
-  manifestShortName: "PersonalBlog", // max 12 characters
+  manifestName: "SpeakUp - An English Blog",
+  manifestShortName: "SpeakUp", // max 12 characters
   manifestStartUrl: "/",
   manifestBackgroundColor: colors.background,
   manifestThemeColor: colors.background,
   manifestDisplay: "standalone",
   // contact
-  contactEmail: "john@doe.com",
+  contactEmail: "speakup@gmail.com",
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "https://facebook.com/greglobinski" }
+    { name: "twitter", url: "https://twitter.com/joshuaedwards" },
+    { name: "facebook", url: "https://facebook.com/joshuaedwards" }
   ]
 };
